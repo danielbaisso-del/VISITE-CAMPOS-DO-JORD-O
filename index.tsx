1d3619082b558309a89f433408f93a27dc502314
DouglasMoraesDev <doougmooraes2024@gmail.com>
2026-01-30 21:37:00 -0300

    teste doug

--- index.tsx
+++ index.tsx
@@ -8,6 +8,12 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
+
+
+
+
+
+
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
